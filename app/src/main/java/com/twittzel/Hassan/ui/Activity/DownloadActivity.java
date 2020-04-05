af7d7dbde806ebf1de779da601a9899e673b3bb3
hassan <roh.bsbs.11@gmail.com>
2020-04-05 11:12:30 +0300

    تم تحسين الاكواد اضافة اكتيفيتي للتحميل

---
+++ app/src/main/java/com/twittzel/Hassan/ui/Activity/DownloadActivity.java
@@ -0,0 +1,16 @@
+package com.twittzel.Hassan.ui.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.twittzel.Hassan.R;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
